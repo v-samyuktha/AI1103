1daef4071a4a9f50528a8b12d4734f0212fec779
v-samyuktha <80803178+v-samyuktha@users.noreply.github.com>
2021-04-06 23:00:55 +0530

    Add files via upload

---
+++ Assignment-5.py
@@ -0,0 +1,17 @@
+#To plot the binomial distribution
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+
+n = 11
+p = 1/3
+r_values = list(range(n + 1))
+# list of pmf values
+dist = [binom.pmf(r, n, p) for r in r_values ]
+# plotting the graph 
+plt.bar(r_values, dist, color ='purple')
+plt.xlabel("value of k")
+plt.ylabel("Pr(X=k)")
+for i, avg in enumerate(dist):
+    plt.annotate('%0.2f' % avg, xy=(i-0.7, avg), xytext=(7,2), color='black', textcoords='offset points')
+plt.title("Binomial distribution of X")
+plt.show()
