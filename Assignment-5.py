1bcb0f82030c696967bf9712fb17605338d07e70
v-samyuktha <80803178+v-samyuktha@users.noreply.github.com>
2021-04-08 14:32:01 +0530

    Update Assignment-5.py

--- Assignment-5.py
+++ Assignment-5.py
@@ -8,9 +8,9 @@ r_values = list(range(n + 1))
 # list of pmf values
 dist = [binom.pmf(r, n, p) for r in r_values ]
 # plotting the graph 
-plt.bar(r_values, dist, color ='purple')
 plt.xlabel("value of k")
 plt.ylabel("Pr(X=k)")
+markerline, stemlines, baseline = plt.stem(r_values, dist, '-.')
 for i, avg in enumerate(dist):
     plt.annotate('%0.2f' % avg, xy=(i-0.7, avg), xytext=(7,2), color='black', textcoords='offset points')
 plt.title("Binomial distribution of X")
